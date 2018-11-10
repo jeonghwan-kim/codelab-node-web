f28fb1db4909356c373a2b93f06c6e9d57b22c72
김정환 <6pack@madup.com>
2018-11-10 21:18:44 +0900

    Middleware를 이용해 Application.use() 메소드 구현

--- src/Application.js
+++ src/Application.js
@@ -1,31 +1,26 @@
 const debug = require('../utils/debug')('Application');
 const http = require('http');
-const serveStatic = require('./serve-static');
-const path = require('path');
-const fs = require('fs');
+const Middleware = require('./Middleware');
 
 
 const Application = () => {
+  const _middleware = Middleware();
+
   const _server = http.createServer((req, res) => {
-    serveStatic(req, res)
-
-    const publicPath = path.join(__dirname, '../public')
-    fs.readFile(`${publicPath}/index.html`, (err, data) => {
-      if (err) throw err
-
-      res.statusCode = 200
-      res.setHeader('Content-Type', 'text/html');
-      res.end(data)
-    })
+    _middleware.run(req, res);
   });
 
+  const use = fn => _middleware.add(fn);
+
   const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
-    _server.listen(port, hostname, fn)
+    _server.listen(port, hostname, fn);
+
     debug('server is listening');
   }
 
   return {
     _server,
+    use,
     listen
   }
 }
