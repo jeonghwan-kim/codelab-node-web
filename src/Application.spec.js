41a268678aa299656ed923c6078fb584e820b29a
김정환 <6pack@madup.com>
2018-11-10 23:28:18 +0900

    Application.use 메소드 테스트 코드 작성

--- src/Application.spec.js
+++ src/Application.spec.js
@@ -2,12 +2,16 @@ require('should');
 const sinon = require('sinon');
 
 const App = require('./Application');
+const Middleware = require('./Middleware');
 
 describe('Application', () => {
+  let app;
+  beforeEach(()=> {
+    app = App()
+  })
   describe('listen()', () => {
     it('server 객체의 listen 함수를 실행한다', () => {
       // arrange
-      const app = App();
       const spy = sinon.spy();
       app._server.listen = spy
 
@@ -18,4 +22,17 @@ describe('Application', () => {
       should(spy.called).be.equal(true);
     });
   });
+
+
+  describe('use()', () => {
+    it('Middleware 모듈 인스턴스의 add() 메소드를 실행한다', () => {
+      const spy = sinon.spy();
+      app._middleware.add = spy;
+      const mw1 = () => null;
+
+      app.use(mw1);
+
+      should(spy.called).be.equal(true);
+    })
+  })
 });
