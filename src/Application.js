6d47645fe466b88f8b0447d8ce4da5f8e94b9eb5
김정환 <6pack@madup.com>
2018-11-10 16:57:13 +0900

    html 정적 리소스 제공

--- src/Application.js
+++ src/Application.js
@@ -1,11 +1,19 @@
 const debug = require('../utils/debug')('Application');
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const Application = () => {
   const _server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.setHeader('Content-Type', 'text/html');
+
+    const filePath = path.join(__dirname, '../public/index.html')
+    fs.readFile(filePath, (err, data) => {
+      if (err) throw err;
+
+      res.end(data);
+    })
   });
 
   const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
