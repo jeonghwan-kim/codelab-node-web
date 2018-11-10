8adf2769356b5caa1803c1aaef916a1e76c93225
김정환 <6pack@madup.com>
2018-11-10 13:19:37 +0900

    bin.js에서 app.js 사용

--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
-const server = require('./server');
+const app = require('./app');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
