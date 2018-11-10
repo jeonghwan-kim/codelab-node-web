7ce48b86364cb3f7a1b31656574a6885208b31d4
김정환 <6pack@madup.com>
2018-11-10 21:12:37 +0900

    Middleware: run() 메소드 구현

--- src/Middleware.js
+++ src/Middleware.js
@@ -3,13 +3,33 @@ const debug = require('../utils/debug')('Middleware');
 const Middleware = () => {
   const _middlewares = [];
 
+  const _run = (i, err) => {
+    if (i < 0 || i >= _middlewares.length) return;
+
+    debug(`i:${i} _middlewares:${_middlewares.length}`)
+
+    const nextMw = _middlewares[i]
+    const next = err => _run(i + 1, err)
+
+    nextMw(_req, _res, next);
+  }
+
+
   const add = fn => {
     _middlewares.push(fn);
+  }
+
+  const run = (req, res) => {
+    _req = req;
+    _res = res;
+
+    _run(0, null);
   }
 
   return {
     _middlewares,
     add,
+    run,
   }
 }
 
