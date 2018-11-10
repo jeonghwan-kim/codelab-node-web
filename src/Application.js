74f0a24ee4eb2e00c52189e8e81e01fb3341501c
김정환 <6pack@madup.com>
2018-11-10 17:29:29 +0900

    모든 정적 파일 제공

--- src/Application.js
+++ src/Application.js
@@ -5,15 +5,32 @@ const fs = require('fs');
 
 const Application = () => {
   const _server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    const mimeType = {
+      '.ico': 'image/x-icon',
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.png': 'image/png',
+      '.jpg': 'image/jpeg',
+      '.eot': 'appliaction/vnd.ms-fontobject',
+      '.ttf': 'aplication/font-sfnt'
+    };
+    const ext = path.parse(req.url).ext;
+    const publicPath = path.join(__dirname, '../public')
 
-    const filePath = path.join(__dirname, '../public/index.html')
-    fs.readFile(filePath, (err, data) => {
-      if (err) throw err;
 
-      res.end(data);
-    })
+    if (Object.keys(mimeType).includes(ext)) {
+      fs.readFile(`${publicPath}${req.url}`, (err, data) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('Not found');
+        } else {
+          res.statusCode = 200
+          res.setHeader('Content-Type', mimeType[ext]);
+          res.end(data)
+        }
+      });
+    }
   });
 
   const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
