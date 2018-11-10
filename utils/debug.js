f0340b94344d5d7e71d59866973fb6d7fb993473
김정환 <6pack@madup.com>
2018-11-10 15:55:17 +0900

    deubg color 추가

--- utils/debug.js
+++ utils/debug.js
@@ -1,11 +1,23 @@
+const colors = [
+  {name: 'cyan',     value: '\x1b[36m'},
+  {name: 'yellow',   value: '\x1b[33m'},
+  {name: 'red',      value: '\x1b[31m'},
+  {name: 'green',    value: '\x1b[32m'},
+  {name: 'magenta',  value: '\x1b[35m'},
+];
+const resetColor = '\x1b[0m';
+
 const debug = tag => {
   if (!tag) throw Error('tag should be required')
 
-  return (...msg) => {
-    const logString = `${tag} ${msg}`;
+  const randIdx = Math.floor(Math.random() * colors.length)
+  const color = colors[randIdx]
+
+  return msg => {
+    const logString = `${color.value}[${tag}]${resetColor} ${msg}`;
     console.log(logString);
     return logString;
   }
 }
 
-module.exports = debug
+module.exports = debug;
