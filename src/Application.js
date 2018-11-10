71a817c846257c9813673a9a1b942b9d9f623b85
김정환 <6pack@madup.com>
2018-11-10 19:29:54 +0900

    serve-static 분리후 문제점

--- src/Application.js
+++ src/Application.js
@@ -1,10 +1,22 @@
 const debug = require('../utils/debug')('Application');
 const http = require('http');
 const serveStatic = require('./serve-static');
+const path = require('path');
+const fs = require('fs');
+
 
 const Application = () => {
   const _server = http.createServer((req, res) => {
     serveStatic(req, res)
+
+    const publicPath = path.join(__dirname, '../public')
+    fs.readFile(`${publicPath}/index.html`, (err, data) => {
+      if (err) throw err
+
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data)
+    })
   });
 
   const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
