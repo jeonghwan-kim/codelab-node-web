a54f76cb0790cfc37e50092b19d384cac00e200d
김정환 <6pack@madup.com>
2018-11-10 12:36:54 +0900

    server 테스트 코드 작성 - listen 메소드 소유 여부 체크

--- server.spec.js
+++ server.spec.js
@@ -1,8 +1,9 @@
-require('should')
-const server = require('./server')
+require('should');
+const server = require('./server');
 
-describe('server test suite', () => {
-  it('should return "hello world"', () => {
-    server().should.be.equal('Hello world')
+describe('server', () => {
+  it('should have listen()', () => {
+    server.should.have.property('listen');
+    should(typeof server.listen).be.equal('function');
   })
 })
