be16df8849330f7f94c970865e02733f941c29c4
김정환 <6pack@madup.com>
2018-11-10 12:43:44 +0900

    bin.js에 서버 구동 코드 추가

---
+++ bin.js
@@ -0,0 +1,7 @@
+const server = require('./server');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
