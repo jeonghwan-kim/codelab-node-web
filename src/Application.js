f037488d69709da352e79820e02c509f201a9a69
김정환 <6pack@madup.com>
2018-11-10 17:31:18 +0900

    serve-static 모듈로 분리

--- src/Application.js
+++ src/Application.js
@@ -1,36 +1,10 @@
 const debug = require('../utils/debug')('Application');
 const http = require('http');
-const path = require('path');
-const fs = require('fs');
+const serveStatic = require('./serve-static');
 
 const Application = () => {
   const _server = http.createServer((req, res) => {
-    const mimeType = {
-      '.ico': 'image/x-icon',
-      '.html': 'text/html',
-      '.js': 'text/javascript',
-      '.css': 'text/css',
-      '.png': 'image/png',
-      '.jpg': 'image/jpeg',
-      '.eot': 'appliaction/vnd.ms-fontobject',
-      '.ttf': 'aplication/font-sfnt'
-    };
-    const ext = path.parse(req.url).ext;
-    const publicPath = path.join(__dirname, '../public')
-
-
-    if (Object.keys(mimeType).includes(ext)) {
-      fs.readFile(`${publicPath}${req.url}`, (err, data) => {
-        if (err) {
-          res.statusCode = 404;
-          res.end('Not found');
-        } else {
-          res.statusCode = 200
-          res.setHeader('Content-Type', mimeType[ext]);
-          res.end(data)
-        }
-      });
-    }
+    serveStatic(req, res)
   });
 
   const listen = (port = 3000, hostname = '127.0.0.1', fn) => {
---
+++ src/serve-static.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+const debug = require('../utils/debug')('serve-static');
+
+const serveStatic = (req, res) => {
+  const mimeType = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.eot': 'appliaction/vnd.ms-fontobject',
+    '.ttf': 'aplication/font-sfnt'
+  };
+  const ext = path.parse(req.url).ext;
+  const publicPath = path.join(__dirname, '../public');
+  debug(`ext: ${ext}`);
+
+  if (Object.keys(mimeType).includes(ext)) {
+    fs.readFile(`${publicPath}${req.url}`, (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end('Not found');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', mimeType[ext]);
+        res.end(data);
+      }
+    });
+  }
+};
+
+module.exports = serveStatic;
