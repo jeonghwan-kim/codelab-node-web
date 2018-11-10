fcb0462abc5e679b0daed8058c510852bd2d9d07
김정환 <6pack@madup.com>
2018-11-10 15:55:02 +0900

    debug color 를 위한 테스트 코드 수정

--- utils/debug.spec.js
+++ utils/debug.spec.js
@@ -6,30 +6,31 @@ describe('debug', () => {
   describe('생성', () => {
     it('태그명을 인자로 받는다 (없으면 예외를 던진다)', () => {
       should(() => debug()).throw();
-    })
+    });
 
     it('함수를 반환한다', () => {
       const debug = require('./debug')('mytag');
       should(typeof debug).be.equal('function');
-    })
+    });
   })
 
   describe('반환된 함수', () => {
-    let debug, tag, msg;
+    let debug, tag, msg, result;
 
     beforeEach(() => {
       tag = 'mytag';
       debug = require('./debug')(tag);
       msg = 'my log message';
-    })
+      result = debug(msg);
+    });
 
-    it('tag + message 형식의 로그 문자열을 반환한다', () => {
-      const expected = `${tag} ${msg}`;
+    it('반환된 문자열은 tag를 포함한다', () => {
+      result.includes(tag).should.be.true;
+    });
 
-      const actual = debug(msg);
-
-      actual.should.be.equal(expected);
-    })
+    it('반환된 문자열은 msg를 포함한다', () => {
+      result.includes(msg).should.be.true;
+    });
 
     it('로그 문자열을 인자로 console.log 함수를 실행한다', () => {
       sinon.spy(console, 'log');
@@ -37,13 +38,7 @@ describe('debug', () => {
 
       debug(msg);
 
-      sinon.assert.calledWith(console.log, expected);
-    })
-  })
-
-  describe('메세지 출력함수', () => {
-    it('_log() 함수 결과문자열을 console.log()함수의 인자로 전달하여 실행한다', () => {
-
-    })
-  })
-})
+      sinon.assert.called(console.log);
+    });
+  });
+});
