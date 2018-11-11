36bff65ecbfaa94d80427fbea4c420b5c37a0b4f
김정환 <6pack@madup.com>
2018-11-11 11:19:12 +0900

    app에서 serve-static 미들웨어 사용

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const debug = require('./utils/debug')('app');
+const serveStatic = require('./middlewares/serve-static');
 const App = require('./src/Application');
 const app = App();
 
+app.use(serveStatic());
 debug('App is initiated');
 
 module.exports = app;
