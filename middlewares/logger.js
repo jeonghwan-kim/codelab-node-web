ab0f148705639119adefdbd4551ac5057e04f00d
김정환 <6pack@madup.com>
2018-11-11 11:54:36 +0900

    logger: 메소드 명에 색상 추가

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,6 +1,27 @@
+const colors = {
+  green: '\x1b[32m',
+  cyan: '\x1b[36m',
+  red: '\x1b[31m',
+  yellow: '\x1b[33m',
+  reset: '\x1b[0m',
+};
+
+const methodColorMap = {
+  get: colors.green,
+  post: colors.cyan,
+  put: colors.yellow,
+  delete: colors.red
+};
+
 const logger = () => (req, res, next) => {
-  const log = `${req.method} ${req.url}`;
+  const coloredMethod = method => {
+    return `${methodColorMap[method.toLowerCase()]}${method}${colors.reset}`;
+  };
+
+  const log = `${coloredMethod(req.method)} ${req.url}`;
+
   console.log(log);
+
   next();
 }
 
