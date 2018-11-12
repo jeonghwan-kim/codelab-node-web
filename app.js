0a0184f8a9cdce023438895670e1a8e5685ce9b5
김정환 <6pack@madup.com>
2018-11-12 19:37:17 +0900

    GET /api/posts 구현

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const serveStatic = require('./middlewares/serve-static');
 const logger = require('./middlewares/logger');
 const errors = require('./middlewares/errors');
 const index = require('./routes/index');
+const apiPost = require('./routes/api/post');
 const App = require('./src/Application');
 const app = App();
 
 app.use(logger());
 app.use(serveStatic());
 app.use('/', index.listPosts());
+app.use('/api/posts', apiPost.index());
 app.use(errors.error404());
 app.use(errors.error());
 
---
+++ routes/api/post.js
@@ -0,0 +1,17 @@
+const debug = require('../../utils/debug')('api/post');
+
+const posts = [
+  {title: 'post 3', body: 'this is post 3'},
+  {title: 'post 2', body: 'this is post 2'},
+  {title: 'post 1', body: 'this is post 1'},
+]
+
+const index = () => (req, res, next) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(posts))
+}
+
+
+module.exports = {
+  index
+}
